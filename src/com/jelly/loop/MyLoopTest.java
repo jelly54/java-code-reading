aac5e6a51f108602f7324863259188dba0924385
zhangguodong <jelly_54@163.com>
2021-10-09 16:45:02 +0800

    字节码:loop循环

---
+++ src/com/jelly/loop/MyLoopTest.java
@@ -0,0 +1,12 @@
+package com.jelly.loop;
+
+public class MyLoopTest {
+    public static int[] numbers = new int[]{1, 2, 3};
+
+    public static void main(String[] args) {
+        ScoreCalculator calculator = new ScoreCalculator();
+        for (int number : numbers) {
+            calculator.record(number);
+        }
+    }
+}
